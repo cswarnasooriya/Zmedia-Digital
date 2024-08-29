73b3a322f47e61cf74db620bde92f54f372db096
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-08-29 15:57:02 +0530

    changes added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,26 +4,26 @@ import Slider from "react-slick";
 const TestimonialData = [
   {
     id: 1,
-    name: "Victor",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    name: "Mr. Saman Indika",
+    text: "I recently hired Zmedia digital solutions for my relocation, and I couldn't be happier with their service. The entire team was professional, punctual, and efficient.",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 2,
-    name: "Satya Nadella",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    name: "Ms. Marry Anjello",
+    text: "I recently hired Zmedia digital solutions for my relocation, and I couldn't be happier with their service. The entire team was professional, punctual, and efficient.",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 3,
-    name: "Virat Kohli",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    name: "Mr. Anjana devage",
+    text: "I recently hired Zmedia digital solutions for my relocation, and I couldn't be happier with their service. The entire team was professional, punctual, and efficient.",
     img: "https://picsum.photos/104/104",
   },
   {
     id: 5,
-    name: "Sachin Tendulkar",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    name: "Mr. Pradeep Silva",
+    text: "I recently hired Zmedia digital solutions for my relocation, and I couldn't be happier with their service. The entire team was professional, punctual, and efficient.",
     img: "https://picsum.photos/103/103",
   },
 ];
@@ -72,26 +72,25 @@ const Testimonials = () => {
       <div className="container">
         {/* header section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto">
-          <p data-aos="fade-up" className="text-sm text-primary">
+          <p data-aos="fade-up" className="text-[24px] dark:text-logo/90 text-logo/80">
             What our customers are saying
           </p>
-          <h1 data-aos="fade-up" className="text-3xl font-bold">
+          <h1 data-aos="fade-up" className="text-6xl font-bold">
             Testimonials
           </h1>
-          <p data-aos="fade-up" className="text-xs text-gray-400">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
-            asperiores modi Sit asperiores modi
+          <p data-aos="fade-up" className="text-[19px] text-gray-600 dark:text-gray-300 text-justify font-bold py-4">
+          We prioritize understanding our clients’ unique needs and crafting strategies that foster meaningful connections with their.
           </p>
         </div>
 
         {/* Testimonial cards */}
         <div data-aos="zoom-in">
           <Slider {...settings}>
-            {TestimonialData.map((data) => (
+            {TestimonialData.map((data) => (<>
               <div className="my-6">
                 <div
                   key={data.id}
-                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative"
+                  className="flex flex-col gap-4 shadow-xl py-10 px-6 text-justify mx-4 rounded-2xl dark:bg-gray-700 bg-primary/30 relative"
                 >
                   <div className="mb-4">
                     <img
@@ -102,17 +101,18 @@ const Testimonials = () => {
                   </div>
                   <div className="flex flex-col items-center gap-4">
                     <div className="space-y-3">
-                      <p className="text-xs text-gray-500">{data.text}</p>
-                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                      <p className="text-[16px] text-black/60 dark:text-gray-200">{data.text}</p>
+                      <h1 className="text-xl font-medium text-black/70 dark:text-light">
                         {data.name}
                       </h1>
                     </div>
                   </div>
-                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                  <p className="text-logo/60 text-9xl font-serif absolute top-0 right-0">
                     ,,
                   </p>
                 </div>
               </div>
+              </>
             ))}
           </Slider>
         </div>
